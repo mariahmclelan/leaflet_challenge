d4970053c916c49b3b815e21bc9e3a94d38dd984
mariahmclelan <mariah.mclelan@yahoo.com>
2024-02-17 18:59:07 -0600

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # leaflet_challenge
+
+The United States Geological Survey, or USGS for short, is responsible for providing scientific data about natural hazards, the health of our ecosystems and environment, and the impacts of climate and land-use change. Using HTML and Java Script I have developed a way to display the data from all earthquakes in the last 30 days. I have added markers and descriptions for each earthquake that has occurred. The colors correspond with the depth of the earthquake and the size correlates with the magnitude of the earthquake.
+
+## references 
+Dataset created by the United States Geological Survey.
---
+++ static/js/logic.js
@@ -0,0 +1,83 @@
+// Define the map
+let myMap = L.map("map", {
+    center: [15.5994, -28.6731],
+    zoom: 2
+});
+
+// Add OpenStreetMap tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
+
+// Define function to create marker style based on earthquake magnitude
+function styleInfo(feature) {
+    return {
+        opacity: 1,
+        fillOpacity: 1,
+        fillColor: markerColor(feature.geometry.coordinates[2]),
+        color: "black",
+        radius: markerSize(feature.properties.mag),
+        stroke: true,
+        weight: 0.5
+    };
+}
+
+// Define function to determine the size of the marker based on earthquake magnitude
+function markerSize(magnitude) {
+    return magnitude * 2;
+}
+
+// Define function to choose color based on earthquake depth
+
+
+
+
+function markerColor(depth) {
+    if (depth > 90) {
+        return '#ffad01';
+    } else if (depth >70) {
+        return '#ff7f00';
+    } else if (depth >50) {
+        return '#ff3401';
+    } else if (depth >30) {
+        return '#f6004d';
+    } else {
+        return "#c50171";
+    }
+}
+
+// Fetch earthquake data from USGS API using D3.js
+let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
+
+d3.json(url).then(function(data) {
+    // Create GeoJSON layer with style and popup
+    L.geoJSON(data, {
+        pointToLayer: function(feature, latlng) {
+            return L.circleMarker(latlng);
+        },
+        style: styleInfo,
+        onEachFeature: function(feature, layer) {
+            layer.bindPopup("<h4>Location: " + feature.properties.place + 
+            "</h4><hr><p>Date & Time: " + new Date(feature.properties.time) + 
+            "</p><hr><p>Magnitude: " + feature.properties.mag + "</p>");
+        }
+    }).addTo(myMap);
+});
+
+//set ledgend
+let legend = L.control({ position: "bottomright" });
+legend.onAdd = function() {
+    let div = L.DomUtil.create("div", "info legend"), 
+    depthLevels = [0, 30, 50, 70, 90];
+
+    div.innerHTML += "<h3>Depth (km)</h3>"
+
+    for (var i = 0; i < depthLevels.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + markerColor(depthLevels[i]) + '"></i> ' +
+            depthLevels[i] + (depthLevels[i + 1] ? '&ndash;' + depthLevels[i + 1] + '<br>' : '+');
+    }
+    return div;
+};
+// Add Legend to the Map
+legend.addTo(myMap);
